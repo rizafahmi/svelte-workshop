a0cad3a1dbe51baabaf761ee45eb6640f98521c8
Riza Fahmi <rizafahmi@gmail.com>
2023-01-03 17:39:36 +0700

    Initial

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from "@sveltejs/adapter-static";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  extensions: [".svelte", ".md"],
+
+  kit: {
+    adapter: adapter(),
+
+    prerender: {
+      default: true,
+      entries: ["*"],
+    },
+  },
+};
+
+export default config;
